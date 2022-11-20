c998d6113e7e8c1e7e62dccc9c47354d61f9577b
Jocadbz <madureirajoaquim95@gmail.com>
2022-11-19 23:18:18 -0300

    Remove deprecated syntax

--- vpass.v
+++ vpass.v
@@ -8,7 +8,7 @@ fn main() {
     mut fp := flag.new_flag_parser(os.args)
     fp.application('vpass')
     fp.version('v1.0.0')
-    fp.limit_free_args(0, 0)?
+    fp.limit_free_args(0, 0)!
     fp.description('This tool generates secure and random password combinations using hexadecimal numbers')
     fp.skip_executable()
     length := fp.int('length', 0, 15, "Password length. 15 letters is the default.")
